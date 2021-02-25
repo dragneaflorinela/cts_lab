45bb29e7a96b2d4cde9c6d766fed1d2f03de645e
dragneaflorinela <dragneaflorinela18@stud.ase.ro>
2021-02-25 09:06:30 +0200

    added hello world example

---
+++ seminar1/src/ro/ase/cts/seminar1/Main.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.seminar1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello cts");
+	}
+
+}
