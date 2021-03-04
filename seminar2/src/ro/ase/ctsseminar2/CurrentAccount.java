5d4bacc9dd2124f49419eadb37bb2536a703756c
dragneaflorinela <dragneaflorinela18@stud.ase.ro>
2021-03-04 08:23:12 +0200

    before break

---
+++ seminar2/src/ro/ase/ctsseminar2/CurrentAccount.java
@@ -0,0 +1,37 @@
+package ro.ase.ctsseminar2;
+
+public class CurrentAccount extends BankAccount {
+
+	
+	public static double MAX_CREDIT=5000;
+	
+	public CurrentAccount() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CurrentAccount(double balance, String iban) {
+		super(balance, iban);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void deposit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transfer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ seminar2/src/ro/ase/ctsseminar2/Main.java
@@ -0,0 +1,13 @@
+package ro.ase.ctsseminar2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//	CurrentAccount c=new CurrentAccount(300,"IBAN1");
+	//System.out.println("suma disponibila:" + c.getBalance());
+		System.out.println("creditul max pt contul curent este" + CurrentAccount.MAX_CREDIT);
+	}
+
+}
